72c3c8ca4a461c4dd0a8d5c71c2fc41e0301ac6e
El Profe Rodo <elproferodofer@gmail.com>
2025-10-07 11:23:22 -0300

    Add files via upload

---
+++ Funciones/04.py
@@ -0,0 +1,11 @@
+"""
+Crear una función que reciba como parámetro la altura
+y el ancho de un rectángulo (en centímetros) para 
+calcular su área. El resultado debe mostrarse por pantalla
+"""
+
+def calcular_area(altura, ancho):
+    area = altura * ancho
+    print(f"El area es {area}")
+
+calcular_area(45, 80)
